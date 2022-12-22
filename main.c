b1920be6cbe189658cd4072ef4a3740e96090fd2
ndiritu-prisca <priscandiritu@outlook.com>
2022-12-22 09:22:28 +0000

    main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+  * main - entry point of the program
+  * @ac: argument count
+  * @av: argument vector
+  * Return: 0 on success
+  */
+
+int main(int ac, char *av[])
+{
+	(void)ac;
+	(void)av;
+	while (1)
+	{
+		char **token;
+		char *buf;
+
+		printf("#cisfun$ ");
+
+
+		buf = line_get();
+		token = tokenize(buf);
+		check_str(buf, token);
+		
+	}
+	return (0);
+}
